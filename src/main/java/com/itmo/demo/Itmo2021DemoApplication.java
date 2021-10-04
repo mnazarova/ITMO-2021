1fb889b2648b92207099845eaa5972738de80140
mnazarova <mnazarova@fkr-spb.ru>
2021-10-04 21:54:59 +0300

    first commit

---
+++ src/main/java/com/itmo/demo/Itmo2021DemoApplication.java
@@ -0,0 +1,21 @@
+package com.itmo.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Itmo2021DemoApplication {
+
+	@GetMapping("/")
+	public  String index() {
+		return "Hello, ITMO! We deployed the project ;) ";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Itmo2021DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itmo/demo/Itmo2021DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itmo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Itmo2021DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
